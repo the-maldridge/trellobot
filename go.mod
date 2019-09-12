fc5c22cf400747b1e4d9fcdd063f7d396227ec2f
Michael Aldridge <aldridge.mac@gmail.com>
2019-09-12 13:24:14 -0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/betterhelp/github_labelbot
+
+go 1.12
+
+require (
+	github.com/google/go-github/v28 v28.1.1
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	gopkg.in/go-playground/webhooks.v5 v5.13.0
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	gapi "github.com/google/go-github/v28/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/go-playground/webhooks.v5/github"
+)
+
+const (
+	path = "/webhooks"
+)
+
+var (
+	webhookSecret, personalAccessToken string
+)
+
+func init() {
+	webhookSecret = os.Getenv("GITHUB_WEBHOOK_SECRET")
+	if webhookSecret == "" {
+		log.Fatal("GITHUB_WEBHOOK_SECRET must be specified!")
+	}
+
+	personalAccessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
+	if personalAccessToken == "" {
+		log.Fatal("GITHUB_ACCESS_TOKEN must be specified!")
+	}
+}
+
+func main() {
+	hook, _ := github.New(github.Options.Secret(webhookSecret))
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: personalAccessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := gapi.NewClient(tc)
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.IssueCommentEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// ok event wasn't one of the ones asked to be parsed
+			}
+		}
+
+		switch payload.(type) {
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+
+			status := &gapi.RepoStatus{
+				State: func() *string {
+					s := "pending"
+					return &s
+				}(),
+				Context: func() *string {
+					s := "trello/attached-card"
+					return &s
+				}(),
+			}
+
+			_, _, err := client.Repositories.CreateStatus(
+				context.Background(),
+				pullRequest.PullRequest.Head.Repo.Owner.Login,
+				pullRequest.PullRequest.Head.Repo.Name,
+				pullRequest.PullRequest.Head.Sha,
+				status,
+			)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		case github.IssueCommentPayload:
+			p := payload.(github.IssueCommentPayload)
+
+			fmt.Println(p.Comment.Body)
+
+			if strings.Contains(p.Comment.Body, "trello.com/c/") {
+				fmt.Printf("Issue #%d is attached to a trello card",
+					p.Issue.Number)
+
+				pr, _, err := client.PullRequests.Get(context.Background(), p.Repository.Owner.Login, p.Repository.Name, int(p.Issue.Number))
+				if err != nil {
+					log.Println("Couldn't get PR", err)
+					return
+				}
+
+				status := &gapi.RepoStatus{
+					State: func() *string {
+						s := "success"
+						return &s
+					}(),
+					Context: func() *string {
+						s := "trello/attached-card"
+						return &s
+					}(),
+				}
+
+				_, _, err = client.Repositories.CreateStatus(
+					context.Background(),
+					p.Repository.Owner.Login,
+					p.Repository.Name,
+					pr.GetHead().GetSHA(),
+					status,
+				)
+				if err != nil {
+					log.Println("Couldn't clear status on PR", p.Issue.Number, err)
+					return
+				}
+			}
+		}
+	})
+	http.ListenAndServe(":3000", nil)
+}
